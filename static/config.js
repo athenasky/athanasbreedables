12ce48f2aa7184de468a0d259f6f15ab05a1a5d4
athenasky <31537571+athenasky@users.noreply.github.com>
2017-09-01 06:35:18 -0400

    Add files via upload

---
+++ static/config.js
@@ -0,0 +1,22 @@
+// ***********************************************************************************
+// ID of the Google spreadsheet
+// ***********************************************************************************
+// Make sure your spreadsheet is published (File > Publish to the web…)
+var spreadsheetId = "1ruB4SVDJybvBv6hzpQX1KqOw0mbu2IwU9ufA8iLmkaU";
+
+// ***********************************************************************************
+// Your FC and IGN
+// ***********************************************************************************
+var friendCode = "0319-2110-2580";
+var inGameName = "Athena";
+
+// ***********************************************************************************
+// URL of your Reddit/Twitter account
+// ***********************************************************************************
+var contactUrl = "http://reddit.com/u/watchmedrive";
+
+// ***********************************************************************************
+// URL of your PGL trainer icon (optional)
+// ***********************************************************************************
+// Download it from the Pokémon Global Link and re-upload to an image-sharing website
+var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/dbac54e0-8ef7-11e7-9da9-06713ea4d7b9.png";
